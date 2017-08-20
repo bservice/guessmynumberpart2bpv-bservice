d0e11615fdea36618f509c24a1edd5fa3274ed99
bservice <brservice@comcast.net>
2017-08-20 19:33:06 -0400

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,54 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	     Scanner console;
+        console = new Scanner(System.in);
+        Random aRandom = new Random(); 
+         
+        int 
+            prev_low = 1, 
+            prev_high = 100, 
+            guess = aRandom.nextInt(prev_high) + prev_low,
+            guesses = 0;
+         
+         
+    //  long time = 0; 
+         
+        boolean win = false;
+         
+        String feedback;
+         
+         
+     
+        while(!win)
+        {
+            System.out.println("My guess is: " + guess);
+            feedback = console.nextLine();
+            //
+             
+            if(feedback.equals("TooLow") || feedback.equals("TooHigh") || feedback.equals("Correct"))
+            {
+                if(feedback.equals("TooLow"))
+                {
+                    prev_low = guess;
+                    System.out.println(feedback);
+                }
+                else if(feedback.equals("TooHigh"))
+                {
+ 
+                    prev_high = guess;
+                    System.out.println(feedback);
+                }
+                else if(feedback.equals("Correct"))
+                {
+                    System.out.print("I win!");
+                    win = true;
+                    System.out.println(feedback);
+                }
+                 
+                guess = aRandom.nextInt(prev_high) + prev_low;
+               
+            }
+        }
     }
 }
